d8608ec859b8d4cc3c9a9cee73591fe66e4d9910
Alen Haric <alen@Alen-MBP.lan1>
2020-10-29 21:48:34 -0400

    adding web, html, bootstrap, and jquery structure

--- test_app/blog/views.py
+++ test_app/blog/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+	{
+		'difficulty': 'hard',
+		'trail_name': 'Devil\'s Hole',
+		'description': 'What a trail...it\'s a slap!',
+		'location': '35.92264 -81.88675'
+	},
+	{
+		'difficulty': 'moderate',
+		'trail_name': 'Looking Glass Rock Trail',
+		'description': 'Makes your legs go \'wooooooooooooo\'',
+		'location': '35.29084 -82.77646'
+	}
+]
+
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+
+	return render(request, 'blog/about.html', {'title':'About'})
+
+
